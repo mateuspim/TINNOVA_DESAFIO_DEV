6bc5bc01fca6e19c83f9b772ffea090246ca9525
Mateus Pim <mateuspimsantos@gmail.com>
2025-08-03 07:49:57 -0300

    Add ForeignKey  to vehicle model

--- exercise-05/backend/app/models/vehicle.py
+++ exercise-05/backend/app/models/vehicle.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, Text
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Text, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
