a6c6f56b8f743245a103f4f5b344bfe25f0616b1
Mateus Pim <mateuspimsantos@gmail.com>
2025-08-03 10:00:40 -0300

    Add GET methods to vehicles router

--- exercise-05/backend/app/routers/vehicle.py
+++ exercise-05/backend/app/routers/vehicle.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Cookie, Response, BackgroundTasks
+from fastapi import APIRouter, Depends, HTTPException, Cookie, Response, Query
 from fastapi_pagination.ext.sqlalchemy import paginate
 from fastapi_pagination import Page
 from sqlalchemy.orm import Session
@@ -23,3 +23,31 @@ router = APIRouter(
     dependencies=[],
     responses={403: {"description": "Not enough permissions"}},
 )
+
+
+@router.get("", response_model=Page[VehicleResponse])
+def get_vehicles(
+    year: int = Query(None),
+    brand_id: int = Query(None),
+    color: str = Query(None),
+    db: Session = Depends(get_db),
+) -> Page[VehicleResponse]:
+    query = db.query(Vehicle)
+    if brand_id is not None:
+        query = query.filter(Vehicle.brand_id == brand_id)
+    if year is not None:
+        query = query.filter(Vehicle.year == year)
+    if color is not None:
+        query = query.filter(Vehicle.color == color)
+
+    query = query.order_by(Vehicle.created_at)
+    return paginate(db, query)
+
+
+@router.get("/{id}", response_model=VehicleResponse)
+def get_vehicle(id: int, db: Session = Depends(get_db)) -> VehicleResponse:
+    vehicle = db.query(Vehicle).filter(Vehicle.id == id).first()
+    if not vehicle:
+        raise HTTPException(404, detail="Vehicle not found")
+
+    return vehicle
