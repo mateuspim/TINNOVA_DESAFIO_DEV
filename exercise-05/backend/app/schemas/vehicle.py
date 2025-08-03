b82c5e40d1da3b6a0ff9479db7db933d0ec36ace
Mateus Pim <mateuspimsantos@gmail.com>
2025-08-03 09:02:32 -0300

    Add Vehicle pydantic schemas

---
+++ exercise-05/backend/app/schemas/vehicle.py
@@ -0,0 +1,45 @@
+from typing import List, Optional, Dict
+from datetime import datetime
+from pydantic import BaseModel
+
+from app.schemas.brand import BrandResponse
+
+
+class VehicleBase(BaseModel):
+    model: str
+    color: str
+    year: int
+    description: Optional[str] = None
+    is_sold: Optional[bool] = False
+
+
+class VehicleCreate(VehicleBase):
+    brand_id: int
+
+
+class VehicleResponse(VehicleBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    brand: BrandResponse
+
+    class Config:
+        from_attributes = True
+
+
+class VehicleUpdate(VehicleBase):
+    model: str
+    color: str
+    year: int
+    description: Optional[str] = None
+    is_sold: Optional[bool] = False
+    brand_id: int
+
+
+class VehiclePatch(BaseModel):
+    model: Optional[str] = None
+    color: Optional[str] = None
+    year: Optional[int] = None
+    description: Optional[str] = None
+    is_sold: Optional[bool] = None
+    brand_id: Optional[int] = None
